784bc1f5c9116ab7983fecc0e5f37752c1338b5f
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-31 10:57:46 +0530

    Initial Commit

--- UltronRoBo/music/plugins/inline.py
+++ UltronRoBo/music/plugins/inline.py
@@ -24,10 +24,10 @@ SOFTWARE.
 
 from pyrogram.handlers import InlineQueryHandler
 from youtubesearchpython import VideosSearch
-from UltronRoBo.utils import USERNAME
+from UltronRoBo.music.utils import USERNAME
 from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, errors
-from UltronRoBo.config import Config
+from UltronRoBo.music.config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
     [
