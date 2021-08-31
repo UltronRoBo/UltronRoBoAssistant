bdb7ae4a5b004d00fe2a1d04b3e2815dac56f237
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-31 10:59:52 +0530

    Initial Commit

--- UltronRoBo/music/userplugins/UltronRoBo.py
+++ UltronRoBo/music/userplugins/UltronRoBo.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from UltronRoBo.utils import *
-from UltronRoBo.config import *
+from UltronRoBo.music.utils import *
+from UltronRoBo.music.config import *
 
 from UltronRoBo.music.userplugins import *
