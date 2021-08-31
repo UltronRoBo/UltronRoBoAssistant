2ab02cc68aacbd048eb6579697ad2c1305849a95
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-31 10:52:45 +0530

    Initial Commit'

--- UltronRoBo/music/plugins/UltronRoBo.py
+++ UltronRoBo/music/plugins/UltronRoBo.py
@@ -21,3 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+from UltronRoBo.utils import *
+from UltronRoBo.config import *
+
+from UltronRoBo.music.plugins import *
