932e59a68d7a7745b128c9dfdbe9e6c5827eaa75
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-09-01 16:16:42 +0530

    Initial Commit

--- UltronRoBo/events.py
+++ UltronRoBo/events.py
@@ -86,3 +86,21 @@ def callbackquery(**args):
         return func
 
     return decorator
+
+
+def UltronLogo(**args):
+    """ For logomaker.py """
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
