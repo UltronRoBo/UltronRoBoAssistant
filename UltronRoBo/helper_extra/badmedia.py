fd2ccaa164329fda79abcbec9c79e9c6357d223d
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-09-01 09:37:30 +0530

    Initial Commit

---
+++ UltronRoBo/helper_extra/badmedia.py
@@ -0,0 +1,61 @@
+"""
+MIT License
+
+Copyright (c) 2021 UltronRoBo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from UltronRoBo import telethn as tbot
+import requests
+import time
+
+
+async def is_nsfw(event):
+    lmao = event
+    if not (
+            lmao.gif
+            or lmao.video
+            or lmao.video_note
+            or lmao.photo
+            or lmao.sticker
+            or lmao.media
+    ):
+        return False
+    if lmao.video or lmao.video_note or lmao.sticker or lmao.gif:
+        try:
+            starkstark = event.client.download_media(lmao.media, thumb=-1)
+        except:
+            return False
+    elif lmao.photo or lmao.sticker:
+        try:
+            starkstark = event.client.download_media(lmao.media)
+        except:
+            return False
+    img = starkstark
+    f = {"file": (img, open(img, "rb"))}
+    
+    r = requests.post("https://starkapi.herokuapp.com/nsfw/", files = f).json()
+    if r.get("success") is False:
+      is_nsfw = False
+    elif r.get("is_nsfw") is True:
+      is_nsfw = True
+    elif r.get("is_nsfw") is False:
+      is_nsfw = False
+    return is_nsfw
