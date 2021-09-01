dc38dba82606e3ac7a5e514c193780963a27dc33
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-09-01 11:41:43 +0530

    Initial Commit

---
+++ UltronRoBo/modules/__texttospeech.py
@@ -0,0 +1,99 @@
+"""
+MIT License
+
+Copyright (c) 2021 UltronRoBo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from UltronRoBo import telethn as tbot
+import os
+
+from gtts import gTTS
+from gtts import gTTSError
+from telethon import *
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from UltronRoBo import *
+
+from UltronRoBo.events import register
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):
+        return True
+
+
+@register(pattern="^/tts (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("🚨 Need Admin Pewer.. You can't use this command.. But you can use in my pm")
+       return
+
+    input_str = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.reply(
+            "Invalid Syntax\nFormat `/tts lang | text`\nFor eg: `/tts en | hello`"
+        )
+        return
+    text = text.strip()
+    lan = lan.strip()
+    try:
+        tts = gTTS(text, tld="com", lang=lan)
+        tts.save("ultron.mp3")
+    except AssertionError:
+        await event.reply(
+            "The text is empty.\n"
+            "Nothing left to speak after pre-precessing, "
+            "tokenizing and cleaning."
+        )
+        return
+    except ValueError:
+        await event.reply("Language is not supported.")
+        return
+    except RuntimeError:
+        await event.reply("Error loading the languages dictionary.")
+        return
+    except gTTSError:
+        await event.reply("Error in Google Text-to-Speech API request !")
+        return
+    with open("ultron.mp3", "r"):
+        await tbot.send_file(
+            event.chat_id, "ultron.mp3", voice_note=True, reply_to=reply_to_id
+        )
+        os.remove("ultron.mp3")
