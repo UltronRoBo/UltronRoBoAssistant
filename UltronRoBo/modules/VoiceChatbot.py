e4b2c6f736166b2d7d24e0870b917f456d7a3d47
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-09-01 10:38:16 +0530

    Initial Commit

---
+++ UltronRoBo/modules/VoiceChatbot.py
@@ -0,0 +1,73 @@
+"""
+MIT License
+
+Copyright (c) 2021 UltronRoBo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import aiofiles
+import aiohttp
+from random import randint
+from pyrogram import filters
+from UltronRoBo import UltronRobo as LYCIA
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
+
+async def ai_lycia(url):
+    ai_name = "Lycia.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(ai_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return ai_name
+
+
+@LYCIA.on_message(filters.command("Ultron"))
+async def Lycia(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("Lycia AI Voice Chatbot")
+        return
+    text = message.text.split(None, 1)[1]
+    lycia = text.replace(" ", "%20")
+    m = await message.reply_text("UltronRoBo Is Best...")
+    try:
+        L = await fetch(f"https://api.affiliateplus.xyz/api/chatbot?message={lycia}&botname=@UltronAssistantRoBot&ownername=@Warning_MadBoy_is_Back&user=1")
+        chatbot = L["message"]
+        VoiceAi = f"https://lyciavoice.herokuapp.com/lycia?text={chatbot}&lang=hi"
+        name = "tesla"
+    except Exception as e:
+        await m.edit(str(e))
+        return
+    await m.edit("Made By @Warning_MadBoy_is_Back")
+    LyciaVoice = await ai_lycia(VoiceAi)
+    await m.edit("Repyping...")
+    await message.reply_audio(audio=LyciaVoice, title=chatbot, performer=name)
+    os.remove(LyciaVoice)
+    await m.delete()
